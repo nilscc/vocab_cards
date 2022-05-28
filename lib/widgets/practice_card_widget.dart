bd7428c8d26df0484502077563b9bcd18eaebf2c
nils.cc <mail@nils.cc>
2022-05-28 20:06:07 +0200

    Fade color of background cards in swipe item stack

--- lib/widgets/practice_card_widget.dart
+++ lib/widgets/practice_card_widget.dart
@@ -5,7 +5,7 @@ import 'package:vocab/cards/box.dart';
 import 'package:vocab/widgets/painter/triangle.dart';
 import 'package:vocab/cards/practice_card.dart';
 
-class PracticeCardWidget extends StatelessWidget {
+class PracticeCardWidget extends StatefulWidget {
   final Box box;
 
   const PracticeCardWidget({
@@ -14,10 +14,17 @@ class PracticeCardWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State createState() => _State();
+}
+
+class _State extends State<PracticeCardWidget> {
+  int _currentItem = 0;
+
+  @override
   Widget build(BuildContext context) {
-    final k = box.stacks.keys.toList();
+    final k = widget.box.stacks.keys.toList();
     k.sort();
-    final c = box.stacks[k.first];
+    final c = widget.box.stacks[k.first];
 
     final swipeItems = c?.map((c) => SwipeItem(content: c)).toList() ?? [];
     final matchEngine = MatchEngine(swipeItems: swipeItems);
@@ -28,22 +35,26 @@ class PracticeCardWidget extends StatelessWidget {
         matchEngine: matchEngine,
         fillSpace: true,
         itemBuilder: (context, idx) {
-          return _card(context, swipeItems[idx].content);
+          return _card(context, swipeItems[idx].content,
+              fade: idx != _currentItem);
         },
         onStackFinished: () {},
+        itemChanged: (item, index) {
+          _currentItem = index;
+        },
       ),
     );
   }
 
-  Widget _card(BuildContext context, PracticeCard card) {
+  Widget _card(BuildContext context, PracticeCard card, {bool fade = false}) {
     return Container(
       padding: const EdgeInsets.all(20),
       child: FlipCard(
         fill: Fill
             .fillBack, // Fill the back side of the card to make in the same size as the front.
         direction: FlipDirection.HORIZONTAL, // default
-        front: _cardContent(context, text: card.front),
-        back: _cardContent(context, text: card.back, front: false),
+        front: _cardContent(context, text: card.front, fade: fade),
+        back: _cardContent(context, text: card.back, fade: fade, front: false),
       ),
     );
   }
@@ -52,12 +63,15 @@ class PracticeCardWidget extends StatelessWidget {
     BuildContext context, {
     required String text,
     bool front = true,
+    bool fade = false,
   }) {
     final theme = Theme.of(context);
     final tt = theme.textTheme;
+    final alpha = fade ? 100 : 255;
 
     return Card(
-      color: front ? Colors.amber.shade800 : Colors.green.shade800,
+      color: (front ? Colors.amber.shade800 : Colors.green.shade800)
+          .withAlpha(alpha),
       child: Column(
         children: [
           Row(
@@ -86,7 +100,10 @@ class PracticeCardWidget extends StatelessWidget {
               alignment: AlignmentDirectional.center,
               child: Text(
                 text,
-                style: tt.caption?.copyWith(color: Colors.white, fontSize: 18),
+                style: tt.caption?.copyWith(
+                  color: Colors.white.withAlpha(alpha),
+                  fontSize: 18,
+                ),
                 overflow: TextOverflow.fade,
               ),
             ),
